d943480026c37b8e942e0fd6378e2a0429fd92d7
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-24 19:43:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,6 @@
 def run_guessing_game
-  random_number = rand(6)+1.to_s
+  random_number = rand(6)+1
+  random_number=random_number.to_s
   puts "Guess a number between 1 and 6."
   input = gets.chomp.to_s
  
